1ad588ed22580c614af265aac7b24f24b0c4709d
Philip Raschke <philip.raschke@tu-berlin.de>
2022-10-04 17:20:08 +0200

    minor changes in model.py; added function export aggregated view on classifier results of datasets by concatenating data frames; updated notebook, now working on export of feature importance plots

--- lib/export.py
+++ lib/export.py
@@ -1,6 +1,5 @@
-import os
-import os
-from os.path import join
+from os import listdir, mkdir
+from os.path import join, exists
 import pandas as pd
 
 def classification_results(results, root):
@@ -24,16 +23,27 @@ def classification_results(results, root):
       is_continuous = results.get(key).get(model).get('continuous')
       pointer = 'continuous' if is_continuous is True else 'category'
 
-      columns[pointer] = results.get(key).get(model).get('train_test').keys()
+      columns[pointer] = [key] + list(results.get(key).get(model).get('train_test').keys())
       index[pointer].append(model)
-      values[pointer].append(results.get(key).get(model).get('train_test').values())
+      values[pointer].append([model] + list(results.get(key).get(model).get('train_test').values()))
     
     for x in ['continuous', 'category']:
       df = pd.DataFrame(values[x], index=index[x], columns=columns[x])
 
       dir = join(root, 'results')
-      if not os.path.exists(dir):
-        os.mkdir(dir)
+      if not exists(dir):
+        mkdir(dir)
 
       filename = key.replace('/', '-') + '-' + x + '.csv'
       df.to_csv(join(dir, filename), sep=',')
+
+def aggregated_classification_results(root):
+  path = join(root, 'results')
+  files = listdir(path)
+
+  for y in ['continuous', 'category']:
+    filtered = list(filter(lambda x: y in x, files))
+    df = pd.concat([pd.read_csv(join(path, x)) for x in filtered], axis=1)
+    df.drop(df.filter(regex="Unname"), axis=1, inplace=True)
+    filename = 'aggregated-' + y + '.csv'
+    df.to_csv(join(path, filename), sep=',')
--- lib/model.py
+++ lib/model.py
@@ -5,19 +5,19 @@ def test(model, continuous, X, y, X_train, X_test, y_train, y_test, features):
   predictions = model.predict(X_test)
 
   result = {
-    "continuous": continuous,
-    "train_test": None,
-    "feature_importance": None,
-    "cross_validation": None
+    'continuous': continuous,
+    'train_test': None,
+    'feature_importance': None,
+    'cross_validation': None
   }
 
   if continuous is True:
-    result["train_test"] = report.continuous(y_test, predictions)
+    result['train_test'] = report.continuous(y_test, predictions)
   else:
-    result["train_test"] = report.category(y_test, predictions)
+    result['train_test'] = report.category(y_test, predictions)
 
-  result["feature_importance"] = report.feature_importance(model, X_test, y_test, features)
-  result["cross_validation"] = report.cross_validation(model, X, y)
+  result['feature_importance'] = report.feature_importance(model, X_test, y_test, features)
+  result['cross_validation'] = report.cross_validation(model, X, y)
 
   output = dict()
   output[type(model).__name__] = result
